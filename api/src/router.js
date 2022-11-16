9e69bbb97acde802568ffc6e931344dddb20e8ca
harsh.singh <hisingh1@gmail.com>
2022-11-16 15:39:15 -0600

    chore : m2

---
+++ api/src/router.js
@@ -0,0 +1,32 @@
+const { Router } = require('express');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename: filename,
+});
+const router = Router();
+
+function filename(request, file, callback) {
+  callback(null, file.originalname);
+}
+
+function fileFilter(request, file, callback) {
+  if (file.mimetype != 'image/png') {
+    request.fileValidationError = 'Wrong file type';
+    callback(null, false, new Error('Wrong file type'));
+  } else {
+    callback(null, true);
+  }
+}
+
+const upload = multer({ fileFilter, storage });
+
+router.post('/upload', upload.single('photo'), (req, res) => {
+  if (req.fileValidationError) {
+    return res.status(400).json({ error: req.fileValidationError });
+  } else {
+    return res.status(201).json({ success: 'true' });
+  }
+});
+
+module.exports = router;
